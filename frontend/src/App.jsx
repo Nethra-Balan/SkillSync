dd1c45249539385c51891384da9aadbe389e3bff
Nethra-Balan <nethrabalan2005@gmail.com>
2025-06-12 10:38:32 +0530

    created login/signup page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { Route, Routes, Navigate } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Signup from "./pages/Signup/Signup";
+import Login from "./pages/Login/Login";
+
+function App() {
+	const user = localStorage.getItem("token");
+
+	return (
+		<Routes>
+			{user && <Route path="/" exact element={<Home />} />}
+			<Route path="/signup" exact element={<Signup />} />
+			<Route path="/login" exact element={<Login />} />
+			<Route path="/" element={<Navigate replace to="/login" />} />
+		</Routes>
+	);
+}
+
+export default App;
